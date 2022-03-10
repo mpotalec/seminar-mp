2447bbe225d83082f4646cf4b7c8b093fd2f4bb3
mpotalec <matko321@hotmail.com>
2022-03-10 12:23:36 +0100

    gotovo

---
+++ index.js
@@ -0,0 +1,129 @@
+//connect to a channel
+const drone = new Scaledrone("7qNd31iHfhCpjACU", {
+    data: {
+        name: getRandomName(),
+        color: getRandomColor(),
+    },
+});
+
+
+function getRandomName() {
+    const adjs = ["skilled", "early", "adamant", "misty", "silent", "empty", "royal", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "blue", "billowing", "broken", "cold", "red", "falling", "green"];
+    const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird"];
+    return (
+      adjs[Math.floor(Math.random() * adjs.length)] +
+      "_" +
+      nouns[Math.floor(Math.random() * nouns.length)]
+    );
+   }
+    
+   function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+   }
+
+let members = [];
+
+//connect to a room
+drone.on('open', error => {
+    if (error) {
+        return console.error(error);
+    }
+    console.log('Sucessfully conected to Scaledrone');
+
+    const room = drone.subscribe('observable-room');
+    room.on('open', error => {
+        if (error) {
+            return console.error(error);
+        }
+        console.log('Sucesfully joined room');
+    });
+
+    room.on('members', m => {
+        members = m;
+        updateMembersDOM();
+    });
+
+    room.on('member_join', member => {
+        members.push(member);
+        updateMembersDOM();
+    });
+
+    room.on('member_leave', ({ id }) => {
+        const index = members.findIndex(member => member.id === id);
+        members.splice(index, 1);
+        updateMembersDOM();
+    });
+
+    room.on('data', (text, member) => {
+        if (member) {
+            addMessageToListDOM(text, member);
+        } else {
+        }
+    });
+});
+
+drone.on('close', event => {
+    console.log('Connection was closed', event);
+});
+
+drone.on('error', error => {
+    console.error(error);
+});
+
+
+//handling users (joining and leaving room)
+const DOM = {
+    membersCount: document.querySelector('.members-count'),
+    membersList: document.querySelector('.members-list'),
+    messages: document.querySelector('.messages'),
+    input: document.querySelector('.message-form__input'),
+    form: document.querySelector('.message-form'),
+};
+
+DOM.form.addEventListener('submit', sendMessage);
+
+function sendMessage() {
+    const value = DOM.input.value;
+    if (value === '') {
+        return;
+    }
+    DOM.input.value = '';
+    drone.publish({
+        room: 'observable-room',
+        message: value,
+    });
+}
+
+function createMemberElement(member) {
+    const { name, color } = member.clientData;
+    const el = document.createElement('div');
+    el.appendChild(document.createTextNode(name));
+    el.className = 'member';
+    el.style.color = color;
+    return el;
+}
+
+function updateMembersDOM() {
+    DOM.membersCount.innerText = `${members.length} users in room:`;
+    DOM.membersList.innerHTML = '';
+    members.forEach(member =>
+        DOM.membersList.appendChild(createMemberElement(member))
+    );
+}
+
+function createMessageElement(text, member) {
+    const el = document.createElement('div');
+    el.appendChild(createMemberElement(member));
+    el.appendChild(document.createTextNode(text));
+    el.className = 'message';
+    return el;
+}
+
+function addMessageToListDOM(text, member) {
+    const el = DOM.messages;
+    const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+    el.appendChild(createMessageElement(text, member));
+    if (wasTop) {
+        el.scrollTop = el.scrollHeight - el.clientHeight;
+    }
+}
